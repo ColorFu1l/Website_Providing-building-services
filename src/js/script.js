213b9097227540b78d87cbeaa2d33d0efbffeba5
Manzhos Vadim <vadim.manzhos@yahoo.com>
2022-09-11 21:37:17 +0300

    Started doing the first project for the portfolio

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.promo__carrousel').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 800,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arr.svg"></div></button>',
+      nextArrow: '<button type="button" class="slick-next"></div><img src="../icons/right_arr.svg"></div></button>',
+    });
+    
+  });
+
+  
